bc9168765555903bf9765ea8f6e94ad015059944
Vladimir Inozemtsev <vladimir.inoz@gmail.com>
2019-02-28 10:47:06 +0300

    Refactoring of workspace - Flickr API is moved to subproject FilckrREST

---
+++ FlickrREST/FlickrREST/FlickrREST.h
@@ -0,0 +1,19 @@
+//
+//  FlickrREST.h
+//  FlickrREST
+//
+//  Created by User on 28/02/2019.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlickrREST.
+FOUNDATION_EXPORT double FlickrRESTVersionNumber;
+
+//! Project version string for FlickrREST.
+FOUNDATION_EXPORT const unsigned char FlickrRESTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlickrREST/PublicHeader.h>
+
+
