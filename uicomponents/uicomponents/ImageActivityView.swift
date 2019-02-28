9bb4a731125dc6fee6f569bef90c22aa6d7ea132
Vladimir Inozemtsev <vladimir.inoz@gmail.com>
2019-02-28 13:24:30 +0300

    Added ImageActivityView

---
+++ uicomponents/uicomponents/ImageActivityView.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+class ImageActivityView: UIView {
+    var imageView: UIImageView!
+    var spinner: UIActivityIndicatorView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        imageView = UIImageView(image: nil)
+        spinner = UIActivityIndicatorView(style: .gray)
+        addSubview(imageView)
+        addSubview(spinner)
+        //spinner is centered at view
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        spinner.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        //image view fills view
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        //show up spinner
+        setImage(image: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setImage(image: nil)
+    }
+    
+    func setImage(image: UIImage?) {
+        if let imageToDisplay = image {
+            spinner.stopAnimating()
+            imageView.image = imageToDisplay
+        } else {
+            spinner.startAnimating()
+            imageView.image = nil
+        }
+    }
+}
--- uicomponents/uicomponents/PhotoCollectionViewCell.swift
+++ uicomponents/uicomponents/PhotoCollectionViewCell.swift
@@ -3,27 +3,20 @@ import UIKit
 /// A collection view cell class that simply displays an image if it exists
 /// or a spinner if image does not exist
 class PhotoCollectionViewCell: UICollectionViewCell {
-    var imageView: UIImageView!
-    var spinner: UIActivityIndicatorView!
+    var imageActivityView: ImageActivityView!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        imageView = UIImageView(image: nil)
-        spinner = UIActivityIndicatorView(style: .gray)
-        addSubview(imageView)
-        addSubview(spinner)
-        //spinner is centered at view
-        spinner.translatesAutoresizingMaskIntoConstraints = false
-        spinner.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-        spinner.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-        //image view fills view
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.topAnchor.constraint(equalTo: topAnchor).isActive = true
-        imageView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
-        imageView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
-        imageView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        imageActivityView = ImageActivityView(frame: CGRect.zero)
+        addSubview(imageActivityView)
+        //view fills superview
+        imageActivityView.translatesAutoresizingMaskIntoConstraints = false
+        imageActivityView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        imageActivityView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        imageActivityView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        imageActivityView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
         //show up spinner
-        setImage(image: nil)
+        imageActivityView.setImage(image: nil)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -32,21 +25,15 @@ class PhotoCollectionViewCell: UICollectionViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        setImage(image: nil)
+        imageActivityView.setImage(image: nil)
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
-        setImage(image: nil)
+        imageActivityView.setImage(image: nil)
     }
     
     func setImage(image: UIImage?) {
-        if let imageToDisplay = image {
-            spinner.stopAnimating()
-            imageView.image = imageToDisplay
-        } else {
-            spinner.startAnimating()
-            imageView.image = nil
-        }
+        imageActivityView.setImage(image: image)
     }
 }
